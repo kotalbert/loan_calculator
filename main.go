dfa1aee2bbb89e890d628280ac25aff18cec8bb3
Pawel Daniluk <pawel.daniluk@kotalbert.net>
2024-02-02 23:31:23 +0100

    Merge pull request #1
    * Add --type flag validation
    * Add validations for flag combinations
    * Implement all flag validation
    * Fix validation for number of flags set

--- main.go
+++ main.go
@@ -3,20 +3,29 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"math"
 )
 
 // CalcOption will be my enum to track what I'm calculating
 type CalcOption int
 
+// todo: include diff type calculation
 const (
 	Payment CalcOption = iota
 	Principal
 	Periods
 )
 
+// I will use this to test if flag is set or not
+const defaultFlagValue = -1
+
 func main() {
-	payment, principal, periods, interest := parseArguments()
+	payment, principal, periods, interest, _, err := parseArguments()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	calcOption := whatCalcWe(payment, principal)
 
@@ -87,13 +96,112 @@ func whatCalcWe(payment *float64, principal *float64) CalcOption {
 	return Periods
 }
 
-func parseArguments() (*float64, *float64, *float64, *float64) {
+func parseArguments() (*float64, *float64, *float64, *float64, *string, error) {
 
-	payment := flag.Float64("payment", -1, "payment amount")
-	principal := flag.Float64("principal", -1, "loan principal")
-	periods := flag.Float64("periods", -1, "number of months needed to repay the loan")
-	interest := flag.Float64("interest", -1, "loan interest")
+	payment := flag.Float64("payment", defaultFlagValue, "payment amount")
+	principal := flag.Float64("principal", defaultFlagValue, "loan principal")
+	periods := flag.Float64("periods", defaultFlagValue, "number of months needed to repay the loan")
+	interest := flag.Float64("interest", defaultFlagValue, "loan interest")
+	calcType := flag.String("type", "", "type of calculation, must be either 'annuity' or 'diff'")
 
 	flag.Parse()
-	return payment, principal, periods, interest
+
+	if err := validateTypeFlag(*calcType); err != nil {
+		return nil, nil, nil, nil, nil, err
+	}
+	if err := validatePaymentFlag(*calcType, *payment); err != nil {
+		return nil, nil, nil, nil, nil, err
+	}
+	if err := validateAllFlagsSetWhenTypeIsDiff(*calcType, *principal, *periods); err != nil {
+		return nil, nil, nil, nil, nil, err
+	}
+	if err := validateAllFlagsSetExceptOneWhenTypeIsAnnuity(*calcType, *principal, *periods, *payment, *interest); err != nil {
+		return nil, nil, nil, nil, nil, err
+	}
+	if err := validateInterestFlag(*interest); err != nil {
+		return nil, nil, nil, nil, nil, err
+
+	}
+	if err := validatePositiveFlagValues(*payment, *principal, *periods, *interest); err != nil {
+		return nil, nil, nil, nil, nil, err
+	}
+
+	return payment, principal, periods, interest, calcType, nil
+}
+
+// validateTypeFlag validation for --type flag;
+//
+//	it should be either 'annuity' or 'diff'
+func validateTypeFlag(calcType string) error {
+	if calcType == "" {
+		return fmt.Errorf("type flag is not set")
+	}
+	if calcType != "annuity" && calcType != "diff" {
+		return fmt.Errorf("invalid type flag value: %s", calcType)
+	}
+	return nil
+}
+
+// validatePaymentFlag validation for --payment flag;
+//
+//	it should not be set when type is diff
+func validatePaymentFlag(calcType string, payment float64) error {
+	if calcType == "diff" && payment != -1 {
+		return fmt.Errorf("payment flag should not be set when type is diff")
+	}
+	return nil
+}
+
+// validateAllFlagsSetWhenTypeIsDiff validation for --type flag == diff;
+//
+//	all flags should be set, except --payment
+func validateAllFlagsSetWhenTypeIsDiff(calcType string, principal float64, periods float64) error {
+	if calcType == "diff" && (principal == defaultFlagValue || periods == defaultFlagValue) {
+		return fmt.Errorf("all flags, except --payment, should be set, when --type is diff")
+	}
+	return nil
+}
+
+// validateAllFlagsSetExceptOneWhenTypeIsAnnuity validation for --type flag == annuity;
+//
+//	exactly 4 of 5 flags should be set
+func validateAllFlagsSetExceptOneWhenTypeIsAnnuity(calcType string, principal float64, periods float64, payment float64, interest float64) error {
+	if calcType == "annuity" {
+		setFlags := 0
+		if principal != defaultFlagValue {
+			setFlags++
+		}
+		if periods != defaultFlagValue {
+			setFlags++
+		}
+		if payment != defaultFlagValue {
+			setFlags++
+		}
+		if interest != defaultFlagValue {
+			setFlags++
+		}
+		if setFlags != 3 {
+			return fmt.Errorf("exactly 4 of 5 flags should be set when --type is annuity")
+		}
+	}
+	return nil
+
+}
+
+func validateInterestFlag(interest float64) error {
+	if interest == defaultFlagValue {
+		return fmt.Errorf("interest flag is not set")
+	}
+	return nil
+}
+
+// validatePositiveFlagValues validation for all the flags;
+//
+//	will check if none of the flags has value less than defaultFlagValue.
+//	potentially it will not catch edge case when value -1 is passed
+func validatePositiveFlagValues(payment float64, principal float64, periods float64, interest float64) error {
+	if payment < defaultFlagValue || principal < defaultFlagValue || periods < defaultFlagValue || interest < defaultFlagValue {
+		return fmt.Errorf("all the values should be positive")
+	}
+	return nil
 }
